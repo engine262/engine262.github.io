1ccb7f358afaf727d3a935223b0a274022b6a3e1
Gus Caplan <me@gus.host>
2019-09-01 22:46:33 -0500

    break importScripts cache on engine262.js

--- src/worker.js
+++ src/worker.js
@@ -5,7 +5,7 @@
 
 importScripts('https://unpkg.com/acorn@7.0.0/dist/acorn.js');
 importScripts('https://unpkg.com/nearley@2.16.0/lib/nearley.js');
-importScripts('https://engine262.js.org/engine262/engine262.js');
+importScripts(`https://engine262.js.org/engine262/engine262.js?_=${Date.now()}`);
 
 const {
   initializeAgent,
